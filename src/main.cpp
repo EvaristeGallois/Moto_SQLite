cdf4b57695c1308218636fd1734aaec04e988e84
Evariste <50232318+EvaristeGallois@users.noreply.github.com>
2025-09-26 17:17:03 +0200

    Mise à jour de main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,30 @@
 #include <string.h>
 #include <windows.h>
 #include <conio.h>
-#include <locale> // Pour utilisation des caractères accentués
+#include <locale> // Pour utilisation des caractÃ¨res accentuÃ©s
 
 #include "console.h"
 #include "menu.h"
 #include "base_CppSqlite3.h"
+
+enum MenuOption {
+    MENU_INVALID = -1,
+    MENU_OPTION_0 = 0,
+    MENU_CREER_OUVRIR = 1,
+    MENU_LISTER = 2,
+    MENU_AJOUTER = 3,
+    MENU_EDITER = 4,
+    MENU_SUPPRIMER = 5,
+    MENU_RENUMEROTER = 6,
+    MENU_QUITTER = 7
+};
 
 int main(int argc, char* argv[])
 {
     int menu_choisi, ret, i;
     bool quitter = false;
 
-    setlocale(LC_CTYPE, "fra"); // Initialisation du jeu de caractères accentués Français
+    setlocale(LC_CTYPE, "fra"); // Initialisation du jeu de caractÃ¨res accentuÃ©s FranÃ§ais
     // redimensionnement de la console
     i = resize_console(140,50);
     //gestion du menu
@@ -31,37 +43,37 @@ int main(int argc, char* argv[])
         menu_choisi = Choix_menu(10,10);
         switch (menu_choisi)
         {
-            case 0:
+            case MENU_OPTION_0:
               // statements
               break;
-            case 1:
+            case MENU_CREER_OUVRIR:
               ret = creation_ouverture_bd();
               break;
-            case 2:
+            case MENU_LISTER:
               ret = lister_bd();
               break;
-            case 3:
+            case MENU_AJOUTER:
               ret = ajouter_element();
               break;
-            case 4:
+            case MENU_EDITER:
               ret = editer_element(); // Dans version CppSqlite
               break;
-            case 5:
+            case MENU_SUPPRIMER:
               ret = supprimer_element();
               break;
-            case 6:
+            case MENU_RENUMEROTER:
               ret = renumeroter_ID_base();
               //ret = reindexer_base();
               break;
-            case 7:
+            case MENU_QUITTER:
               quitter=true;
               break;
             default:
-                //
+                std::cerr << "Option invalide : " << menu_choisi << std::endl;
                 break;
         }
     }
-    // Fermeture de la base de donnée
+    // Fermeture de la base de donnÃ©e
     ret = fermer_bd();
     clear_console();
     //i = resize_console(120,50);
